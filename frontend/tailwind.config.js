f557bbca95c9065d43cd7cc4bff6dbf6f7694f4b
Abdurahman <abdi_mo4@outlook.com>
2025-08-24 10:27:40 +0100

    setup react

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}", // no ts/tsx yet
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
